8387e56a240eac80388d3232eb4a45c2c3225213
Saish Sankhe <saishsankhe23@gmail.com>
2021-05-05 16:37:13 -0400

    create login form, check on submit

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,11 @@ import Login from './components/Login';
 import CreateStory from './components/CreateStory';
 import StoryList from './components/StoryList';
 import StoryReview from './components/StoryReview';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 function App() {
 	return (
 		<Router>
-			<div className="App">
-				<p>Helllo</p>
-			</div>
 			<Link className="showlink" to="/login">
 				Login
 			</Link>
@@ -26,11 +23,14 @@ function App() {
 			<Link className="showlink" to="/story-review/:id">
 				Each story
 			</Link>
-			<Route exact path="/login" component={Login} />
-			<Route exact path="/create-story" component={CreateStory} />
-			<Route exact path="/story-list" component={StoryList} />
-			<Route exact path="/story-review" component={StoryReview} />
-			<Route exact path="/story-review/:id" component={StoryReview} />
+
+			<Switch>
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/create-story" component={CreateStory} />
+				<Route exact path="/story-list" component={StoryList} />
+				<Route exact path="/story-review" component={StoryReview} />
+				<Route exact path="/story-review/:id" component={StoryReview} />
+			</Switch>
 		</Router>
 	);
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,46 @@
+import axios from 'axios';
+
 const Login = () => {
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const data = e.target;
+		const loginObj = {
+			email: data.email.value,
+			password: data.pass.value,
+			isAdmin: data.role.value === 'admin' ? true : false,
+		};
+
+		const response = await axios.post('/api/v1/signin', loginObj);
+
+		const token = response.data.token;
+
+		console.log(token);
+	};
+
 	return (
 		<div>
-			<p>Login page</p>
+			<form className="myForm" method="POST" onSubmit={handleSubmit}>
+				<label>
+					Email
+					<input type="email" id="email" required></input>
+				</label>
+				<label>
+					Password
+					<input type="password" id="pass" required></input>
+				</label>
+				<label>
+					Role
+					<label>
+						<input type="radio" id="user" name="role" value="user" required />
+						User
+					</label>
+					<label>
+						<input type="radio" id="admin" name="role" value="admin" required />
+						Admin
+					</label>
+				</label>
+				<button type="submit">Login</button>
+			</form>
 		</div>
 	);
 };
