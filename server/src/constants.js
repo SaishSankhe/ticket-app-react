8288cce004a1bd4dbeb77d44ca6b766a9a56940e
Saish Sankhe <saishsankhe23@gmail.com>
2021-05-05 13:59:20 -0400

    created server client folders, added server files

---
+++ server/src/constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+	auth: {
+		privateKey: '1e7a4c07-0b02-4a03-97b2-4f003d84d886',
+	},
+	roles: {
+		admin: 'Admin',
+		user: 'user',
+	},
+	storyTypes: ['enhancement', 'bugfix', 'development', 'qa'],
+	storyComplexity: ['low', 'mid', 'high'],
+};
