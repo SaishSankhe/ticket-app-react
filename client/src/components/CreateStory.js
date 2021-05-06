639de8b7bd72bce0ddc374e5631e521e29fb3a6a
Saish Sankhe <saishsankhe23@gmail.com>
2021-05-05 20:55:10 -0400

    added add story form, get story list from backend

--- client/src/components/CreateStory.js
+++ client/src/components/CreateStory.js
@@ -1,21 +1,81 @@
 import { withRouter } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../AuthContext';
+import axios from 'axios';
 
-const CreateStory = () => {
+const CreateStory = (props) => {
 	const [details] = useContext(AuthContext);
 
-	const printToken = () => {
-		if (details) {
-			console.log(details);
-		}
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const data = e.target;
+
+		const storyObj = {
+			summary: data.summary.value,
+			description: data.description.value,
+			type: data.type.value,
+			complexity: data.complexity.value,
+		};
+		console.log(details.token);
+		await axios.post('/api/v1/stories', storyObj, {
+			headers: {
+				Authorization: `Bearer ${details.token}`,
+			},
+		});
+
+		props.history.push('/story-list');
 	};
-	return (
-		<div>
-			<p>Create story page</p>
-			{printToken()}
-		</div>
-	);
+
+	const addStoryForm = () => {
+		return (
+			<form className="myForm" onSubmit={handleSubmit}>
+				<label>
+					Summary:
+					<input type="text" id="summary" required />
+				</label>
+				<label>
+					Description:
+					<textarea id="description" rows="3" required />
+				</label>
+				<label>
+					Type:
+					<select name="type" id="type" required>
+						<option disabled value="none" selected="selected">
+							Select story type
+						</option>
+						<option value="enhanced">Enhanced</option>
+						<option value="bugfix">Bugfix</option>
+						<option value="development">Development</option>
+						<option value="qa">QA</option>
+					</select>
+				</label>
+				<label>
+					Complexity:
+					<select name="complexity" id="complexity" required>
+						<option disabled selected="selected" value="none">
+							Select complexity
+						</option>
+						<option value="low">Low</option>
+						<option value="mid">Medium</option>
+						<option value="high">High</option>
+					</select>
+				</label>
+				<label>
+					Estimated time:
+					<input type="number" id="estimated" required />
+				</label>
+				<label>
+					Cost:
+					<span className="currencyinput">
+						$<input type="number" name="currency" id="cost" required />
+					</span>
+				</label>
+				<button type="submit">Add story</button>
+			</form>
+		);
+	};
+
+	return <div>{addStoryForm()}</div>;
 };
 
 export default withRouter(CreateStory);
--- client/src/components/StoryList.js
+++ client/src/components/StoryList.js
@@ -1,9 +1,26 @@
 import { withRouter } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../AuthContext';
+import axios from 'axios';
 
 const StoryList = () => {
 	const [details] = useContext(AuthContext);
+	const [stories, setStories] = useState({});
+
+	useEffect(() => {
+		async function getStories() {
+			const getStories = await axios.get('/api/v1/stories', {
+				headers: {
+					Authorization: `Bearer ${details.token}`,
+				},
+			});
+
+			setStories(getStories);
+		}
+		getStories();
+	}, []);
+
+	console.log(stories);
 
 	return (
 		<div>
