9a819e03c971a1ee7086937461a3fb76420622a6
Saish Sankhe <saishsankhe23@gmail.com>
2021-05-06 03:17:23 -0400

    added sort, filter, local storage for refresh

--- client/src/AuthContext.js
+++ client/src/AuthContext.js
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
-	const [details, setDetails] = useState({});
+	const localState = JSON.parse(localStorage.getItem('details'));
+
+	const [details, setDetails] = useState(
+		{ token: localState.token, role: localState.role } || {}
+	);
+
+	useEffect(() => {
+		localStorage.setItem('details', JSON.stringify(details));
+	}, [details]);
 
 	return (
 		<AuthContext.Provider value={[details, setDetails]}>
--- client/src/components/StoryList.js
+++ client/src/components/StoryList.js
@@ -7,6 +7,7 @@ import Navigation from './Navigation';
 const StoryList = () => {
 	const [details] = useContext(AuthContext);
 	const [stories, setStories] = useState([]);
+	const [filter, setFilter] = useState('none');
 
 	useEffect(() => {
 		async function getStories() {
@@ -21,21 +22,98 @@ const StoryList = () => {
 		getStories();
 	}, [details]);
 
+	const sortById = () => {
+		const sortedStories = [].concat(stories).sort((a, b) => {
+			return a.id - b.id;
+		});
+
+		setStories(sortedStories);
+	};
+
+	const sortByComplexity = () => {
+		const type = ['high', 'mid', 'low'];
+		const sortedStories = [].concat(stories).sort((a, b) => {
+			return type.indexOf(a.complexity) - type.indexOf(b.complexity);
+		});
+
+		setStories(sortedStories);
+	};
+
+	const filterByType = (e) => {
+		setFilter(e.target.value);
+	};
+
+	const filteredStories = () => {
+		const filteredStories = stories.filter((item) => item.type === filter);
+
+		return (
+			<div>
+				{filteredStories.map((item, index) => (
+					<div key={index}>
+						<p>{item.id}</p>
+						<p>{item.summary}</p>
+						<p>{item.description}</p>
+						<p>{item.type}</p>
+						<p>{item.complexity}</p>
+						<p>{item.estimatedHrs}</p>
+						<p>{item.cost}</p>
+						<hr />
+					</div>
+				))}
+			</div>
+		);
+	};
+
 	return (
 		<div>
 			<Navigation />
-			{stories.map((item, index) => (
-				<div key={index}>
-					<p>{item.id}</p>
-					<p>{item.summary}</p>
-					<p>{item.description}</p>
-					<p>{item.type}</p>
-					<p>{item.complexity}</p>
-					<p>{item.estimatedHrs}</p>
-					<p>{item.cost}</p>
-					<hr />
-				</div>
-			))}
+			<button
+				type="button"
+				onClick={() => {
+					sortById();
+				}}
+			>
+				Sort by ID
+			</button>
+			<button
+				type="button"
+				onClick={() => {
+					sortByComplexity();
+				}}
+			>
+				Sort by Type
+			</button>
+			<form>
+				<label>
+					Filter by type
+					<select
+						name="type"
+						id="filterValue"
+						defaultValue="none"
+						onChange={filterByType}
+					>
+						<option value="none">None</option>
+						<option value="enhancement">Enhancement</option>
+						<option value="bugfix">Bugfix</option>
+						<option value="development">Development</option>
+						<option value="qa">QA</option>
+					</select>
+				</label>
+			</form>
+			{filter === 'none'
+				? stories.map((item, index) => (
+						<div key={index}>
+							<p>{item.id}</p>
+							<p>{item.summary}</p>
+							<p>{item.description}</p>
+							<p>{item.type}</p>
+							<p>{item.complexity}</p>
+							<p>{item.estimatedHrs}</p>
+							<p>{item.cost}</p>
+							<hr />
+						</div>
+				  ))
+				: filteredStories()}
 		</div>
 	);
 };
